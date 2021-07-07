ed84fd2bf07bc1a635bc188539de66b2fc91c5ba
Nikhil Rao <raonikhil57@gmail.com>
2021-07-07 09:51:44 -0700

    initial commit

---
+++ src/com/tapcart/Main.java
@@ -0,0 +1,29 @@
+package com.tapcart;
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.util.Base64;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Main {
+    public static String readFileAsString(String file)throws Exception
+    {
+        return new String(Files.readAllBytes(Paths.get(file)));
+    }
+
+    public static void main(String[] args) {
+        try {
+            String file = args[1];
+            String json = readFileAsString(file);
+            String secret = args[0];
+            byte[] hmacSha256 = HMAC.calcHmacSha256(secret.getBytes("UTF-8"), json.getBytes("UTF-8"));
+            System.out.println(String.format("Hex: %032x", new BigInteger(1, hmacSha256)));
+            String base64HmacSha256 = Base64.getEncoder().encodeToString(hmacSha256);
+            System.out.println("Base64: " + base64HmacSha256);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
